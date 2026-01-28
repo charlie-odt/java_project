f4535526ac89a6219f2189e942a7df602b0743cd
valeriomarchese14 <valerio.marchese140402@gmail.com>
2026-01-28 20:03:29 +0100

    Create ConnexionBdd.java

---
+++ eclipse-workspace/java_project/src/java_project/ConnexionBdd.java
@@ -0,0 +1,34 @@
+package java_project
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnexionBdd {
+    private static final String URL = "jdbc:mysql://localhost:3306/maintenance_app";
+    private static final String USER = "etudiant";
+    private static final String PASSWORD = "etudiant";
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                } catch (ClassNotFoundException e) {
+                    System.err.println("Driver MySQL introuvable");
+                    e.printStackTrace();
+                    return null;
+                }
+
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Connexion à la BDD réussie");
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur de connexion à la BDD");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
